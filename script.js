905975cf806be281cebe4eec04a2a8de618d852a
Adrián Vera <vera.adrianj@gmail.com>
2016-09-21 12:31:25 -0430

    Mejoras

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
- function consultar(){
-      var cedula=document.getElementById('cedula').value;
-      var expediente=document.getElementById('expediente').value;
-      cedula=parseInt(cedula);
-      expediente=parseInt(expediente);
+function consultar(){
+      var cedula=document.getElementById('cedula').value
+      var expediente=document.getElementById('expediente').value
+      cedula=parseInt(cedula)
+      expediente=parseInt(expediente)
 
-      var url="http://servicios.uneg.edu.ve/pregradoweb/inscripcion/sce110_pdf.php?PHPSESSID=e663143104c420bea1d3323d2a8ac542&apb_cedula=V"+cedula+"&apb_exp="+expediente+"";
-        alert(url);
-      var a = document.createElement("a");
-      a.target = "_blank";
-      a.href =url;
-      a.click();
+      var url="http://servicios.uneg.edu.ve/pregradoweb/inscripcion/sce110_pdf.php?PHPSESSID=3e0627800ff2fa33ed5494557fe7ff12&apb_cedula=V"+cedula+"&apb_exp="+expediente+""
+      var a = document.createElement("a")
+      a.target = "_blank"
+      a.href =url
+      a.click()
 }
